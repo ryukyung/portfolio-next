aa0be65974a436b6f30756846fda2fd7a86e542a
ryukyunghye <ryukyung123@naver.com>
2023-05-15 06:55:05 +0900

    :speech_balloon: 텍스트 수정

--- components/home/hero.js
+++ components/home/hero.js
@@ -1,10 +1,12 @@
+import Link from 'next/link';
+
 export default function Hero() {
   return (
     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
       <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-        안녕하세요 가오리입니다.
+        Hello!
         <br className="hidden lg:inline-block" />
-        welcome to gaori world
+        FrontEnd Developer Gaori
       </h1>
       <p className="mb-8 leading-relaxed">
         심장은 미인을 이상 그들은 찬미를 꾸며 하는 철환하였는가? 봄날의 거친
@@ -17,9 +19,11 @@ export default function Hero() {
         봄바람이다.
       </p>
       <div className="flex justify-center">
-        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-          see project
-        </button>
+        <Link
+          href="/projects"
+          className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          View Project
+        </Link>
       </div>
     </div>
   );
