80566a432f71688812ec9d993242efdfc8e717d8
ryukyunghye <ryukyung123@naver.com>
2023-05-15 06:20:01 +0900

    :construction: hero

---
+++ components/home/hero.js
@@ -0,0 +1,26 @@
+export default function Hero() {
+  return (
+    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+        안녕하세요 가오리입니다.
+        <br className="hidden lg:inline-block" />
+        welcome to gaori world
+      </h1>
+      <p className="mb-8 leading-relaxed">
+        심장은 미인을 이상 그들은 찬미를 꾸며 하는 철환하였는가? 봄날의 거친
+        이것이야말로 사람은 말이다. 인생에 청춘을 청춘은 평화스러운 약동하다.
+        놀이 용감하고 충분히 곳으로 청춘이 가슴에 희망의 찾아다녀도, 것이다.
+        거선의 찾아다녀도, 기관과 때에, 그러므로 그리하였는가? 산야에 보이는
+        새가 쓸쓸하랴? 목숨이 이상은 못할 생생하며, 천자만홍이 뼈 길지
+        부패뿐이다. 든 있는 얼마나 고행을 길지 긴지라 인류의 끓는다. 없는 크고
+        끝에 남는 같이, 얼음이 철환하였는가? 앞이 스며들어 바이며, 이상은
+        봄바람이다.
+      </p>
+      <div className="flex justify-center">
+        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          see project
+        </button>
+      </div>
+    </div>
+  );
+}
