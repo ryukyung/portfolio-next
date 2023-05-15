d685459d13d717528cac78001335d3824b5b6b0c
ryukyunghye <ryukyung123@naver.com>
2023-05-15 09:51:46 +0900

    :sparkles: darkmode

--- components/header.js
+++ components/header.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import DarkModeToggleButton from './dark-mode-toggle-button';
+
 export default function Header() {
   return (
     <>
@@ -24,19 +26,8 @@ export default function Header() {
               Blog
             </Link>
           </nav>
-          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-            Button
-            <svg
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-4 h-4 ml-1"
-              viewBox="0 0 24 24">
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </button>
+          <DarkModeToggleButton />
+          {/* 다크모드 버튼 */}
         </div>
       </header>
     </>
