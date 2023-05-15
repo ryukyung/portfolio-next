4569fd3f435e4297e43769863126104c6b3489ef
ryukyunghye <ryukyung123@naver.com>
2023-05-15 20:43:30 +0900

    :construction: index

---
+++ pages/projects.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import Layout from '@/components/layout';
+
+export default function Projects() {
+  return (
+    <Layout>
+      <Head>
+        <title>gaori's portfolio</title>
+        <meta name="description" content="gaori's portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>프로젝트</h1>
+    </Layout>
+  );
+}
